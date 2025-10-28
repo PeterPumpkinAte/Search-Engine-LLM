82062888dfa2fa9fc9d801eef98ad618eb049991
Pedro De La Fuente <71839926+PeterPumpkinAte@users.noreply.github.com>
2025-10-28 13:18:47 -0400

    Refactor agent initialization and execution

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from langchain_groq import ChatGroq
 from langchain_community.utilities import ArxivAPIWrapper,WikipediaAPIWrapper
 from langchain_community.tools import ArxivQueryRun,WikipediaQueryRun,DuckDuckGoSearchRun
-from langchain.agents import initialize_agent,AgentType
+from langchain.agents import create_react_agent, AgentExecutor
 from langchain.callbacks import StreamlitCallbackHandler
 import os
 from dotenv import load_dotenv
@@ -44,11 +44,16 @@ if prompt:=st.chat_input(placeholder="What is machine learning?"):
     llm=ChatGroq(groq_api_key=api_key,model_name="Llama3-8b-8192",streaming=True)
     tools=[search,arxiv,wiki]
 
-    search_agent=initialize_agent(tools,llm,agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION,handling_parsing_errors=True)
-
+    search_agent = create_react_agent(
+        tools=tools,
+        llm=llm,
+    )
+    agent_executor = AgentExecutor(agent=search_agent, tools=tools, verbose=True)
+    
     with st.chat_message("assistant"):
-        st_cb=StreamlitCallbackHandler(st.container(),expand_new_thoughts=False)
-        response=search_agent.run(st.session_state.messages,callbacks=[st_cb])
-        st.session_state.messages.append({'role':'assistant',"content":response})
-        st.write(response)
+        st_cb = StreamlitCallbackHandler(st.container(), expand_new_thoughts=False)
+        response = agent_executor.invoke({"input": prompt}, callbacks=[st_cb])
+        st.session_state.messages.append({'role': 'assistant', "content": response["output"]})
+        st.write(response["output"])
+
 
