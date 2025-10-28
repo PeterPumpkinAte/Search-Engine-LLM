f6271cc32ba2bc86b7082132c0c885b07a7ebf77
Pedro De La Fuente <71839926+PeterPumpkinAte@users.noreply.github.com>
2025-10-28 14:07:46 -0400

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-
+#
 arxiv_wrapper = ArxivAPIWrapper(top_k_results=1, doc_content_chars_max=200)
 arxiv = ArxivQueryRun(api_wrapper=arxiv_wrapper)
 api_wrapper = WikipediaAPIWrapper(top_k_results=1, doc_content_chars_max=200)
@@ -41,4 +41,5 @@ if prompt := st.chat_input(placeholder="What is machine learning?"):
     response = llm.invoke(prompt)
     st.session_state["messages"].append({'role': 'assistant', 'content': response})
     st.chat_message("assistant").write(response)
+
 
