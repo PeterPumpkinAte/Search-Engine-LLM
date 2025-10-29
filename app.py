83a2c6d19dff49d4f93ae4a1dcb8631949ea5f89
Pedro De La Fuente <71839926+PeterPumpkinAte@users.noreply.github.com>
2025-10-29 16:53:40 -0400

    Fix formatting issues in app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-#
+###
 arxiv_wrapper = ArxivAPIWrapper(top_k_results=1, doc_content_chars_max=200)
 arxiv = ArxivQueryRun(api_wrapper=arxiv_wrapper)
 api_wrapper = WikipediaAPIWrapper(top_k_results=1, doc_content_chars_max=200)
